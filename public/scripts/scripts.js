122f1b830f61aa5a66779bd88f331dcfae34bb8c
David Stinnett <davidstinnett@icloud.com>
2020-12-09 21:50:15 -0500

    maybe fixed mobile styling

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,12 +1,9 @@
-$(function () {
-
-	$(window).bind("resize", function () {
-		console.log($(this).width())
-		if ($(this).width() < 900) {
-			$('#jumbotron').removeClass('top-50').removeClass('start-50').removeClass('translate-middle').addClass("top-0").addClass("start-50").addClass("translate-middle-x")
-		}
-		else {
-			$('#jumbotron').removeClass("top-0").removeClass("start-50").removeClass("translate-middle-x").addClass('top-50').addClass('start-50').addClass('translate-middle')
-		}
-	})
+$(document).ready(function () {
+	console.log($(this).width())
+	if ($(this).width() < 900) {
+		$('#jumbotron').removeClass('top-50').removeClass('start-50').removeClass('translate-middle').addClass("top-0").addClass("start-50").addClass("translate-middle-x")
+	}
+	else {
+		$('#jumbotron').removeClass("top-0").removeClass("start-50").removeClass("translate-middle-x").addClass('top-50').addClass('start-50').addClass('translate-middle')
+	}
 })
