1bdfea17aa99c28cc5d358cfd7eab8a2fc3e57cb
David Stinnett <davidstinnett@icloud.com>
2020-12-10 22:10:14 -0500

    styling on index page

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,11 +1,11 @@
 
-// //Changes bootstrap classes on main page so jumbotron doesn't jump over navbar on mobile
-// $(document).ready(function () {
-// 	console.log($(this).width())
-// 	if ($(this).width() < 900) {
-// 		$('#jumbotron').removeClass('top-50').removeClass('start-50').removeClass('translate-middle').addClass("top-0").addClass("start-50").addClass("translate-middle-x")
-// 	}
-// 	else {
-// 		$('#jumbotron').removeClass("top-0").removeClass("start-50").removeClass("translate-middle-x").addClass('top-50').addClass('start-50').addClass('translate-middle')
-// 	}
-// })
+//Changes bootstrap classes on main page so jumbotron doesn't jump over navbar on mobile
+$(document).ready(function () {
+	console.log($(this).width())
+	if ($(this).width() < 900) {
+		$('#jumbotron').removeClass('top-50').removeClass('start-50').removeClass('translate-middle').addClass("top-0").addClass("start-50").addClass("translate-middle-x")
+	}
+	else {
+		$('#jumbotron').removeClass("top-0").removeClass("start-50").removeClass("translate-middle-x").addClass('top-50').addClass('start-50').addClass('translate-middle')
+	}
+})
