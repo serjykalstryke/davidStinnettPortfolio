74a046886b3973c5e13e5cdcfacd87a26fdb2d0c
David Stinnett <davidstinnett@icloud.com>
2023-05-31 22:49:46 -0400

    update npm installs to allow for updating heroku deploy

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React from "react"
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas, faSatellite } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import './App.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import NavBar from "./components/layout/NavBar"
+import "./App.css";
+import NavBar from "./components/layout/NavBar";
 
+import Home from "./components/pages/Home";
 
-import Home from "./components/pages/Home"
+import Portfolio from "./components/pages/Portfolio";
 
-import Portfolio from "./components/pages/Portfolio"
-
-
-library.add(fas)
+library.add(fas);
 
 function App() {
   return (
     <Router>
-      <div className="s p-1 dot"><FontAwesomeIcon icon ={["fas", "satellite"]} size="1x"/></div>
+      <div className="s p-1 dot">
+        <FontAwesomeIcon icon={["fas", "satellite"]} size="1x" />
+      </div>
       <NavBar />
-      <Jumbotron className="jumbotron">
-      <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/Portfolio" component={Portfolio} />
-      </Switch>
-      </Jumbotron>
-    
+      <div className="jumbotron container-fluid py-5">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Portfolio" element={<Portfolio />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
