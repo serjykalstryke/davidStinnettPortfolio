6b70e11743162524c66abfbe558a93987c6d91a2
David Stinnett <davidstinnett@icloud.com>
2023-05-31 23:51:09 -0400

    added more updated resume

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ContactModal from '../contact/ContactModal';
-import Hamburger from './Hamburger';
-import Logo from './Logo';
-import resume from "../../resume/DavidStinnettResume.pdf"
+import ContactModal from "../contact/ContactModal";
+import Hamburger from "./Hamburger";
+import Logo from "./Logo";
+import resume from "../../resume/David+Stinnett_Resume+2023.docx";
 
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { LinkContainer } from 'react-router-bootstrap';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
 export const NavBar = () => {
   const [mobileNavOpened, setMobileNavOpened] = useState(false);
@@ -16,32 +16,32 @@ export const NavBar = () => {
   //* Dismiss navbar on link click
 
   return (
-    <Navbar className={mobileNavOpened ? 'nav dropdown-opened' : 'nav'}>
-      <Navbar.Brand className='logo hideMobile' href='/'>
-          <Logo logo="../img/logo.png" logoAlt="David's logo" />
+    <Navbar className={mobileNavOpened ? "nav dropdown-opened" : "nav"}>
+      <Navbar.Brand className="logo hideMobile" href="/">
+        <Logo logo="../img/logo.png" logoAlt="David's logo" />
       </Navbar.Brand>
 
       <Hamburger toggleMobileNav={toggleMobileNav} opened={mobileNavOpened} />
 
-      <div className='dropdown-link-container text'>
-        <div className='dropdown-links'>
-          <LinkContainer to='/'>
-            <Nav.Link className='links text' onClick={toggleMobileNav}>
+      <div className="dropdown-link-container text">
+        <div className="dropdown-links">
+          <LinkContainer to="/">
+            <Nav.Link className="links text" onClick={toggleMobileNav}>
               Home
             </Nav.Link>
           </LinkContainer>
 
-          <LinkContainer to='/portfolio'>
-            <Nav.Link className='links text' onClick={toggleMobileNav}>
+          <LinkContainer to="/portfolio">
+            <Nav.Link className="links text" onClick={toggleMobileNav}>
               Portfolio
             </Nav.Link>
           </LinkContainer>
 
           <Nav.Link
-            className='links text'
+            className="links text"
             href={resume}
-            target='_blank'
-            rel='noreferrer noopener'
+            target="_blank"
+            rel="noreferrer noopener"
             download
           >
             Resume
@@ -49,24 +49,24 @@ export const NavBar = () => {
           <ContactModal />
 
           <Nav.Link
-            href='https://github.com/serjykalstryke'
-            target='_blank'
-            className='links'
-            rel='noreferrer noopener'
+            href="https://github.com/serjykalstryke"
+            target="_blank"
+            className="links"
+            rel="noreferrer noopener"
           >
-            <span className='icon'>
-              <i className='fab fa-github text links'></i>
+            <span className="icon">
+              <i className="fab fa-github text links"></i>
             </span>
           </Nav.Link>
 
           <Nav.Link
-            href='https://www.linkedin.com/in/david-stinnett-259262166/'
-            target='_blank'
-            className='links'
-            rel='noreferrer noopener'
+            href="https://www.linkedin.com/in/david-stinnett-259262166/"
+            target="_blank"
+            className="links"
+            rel="noreferrer noopener"
           >
-            <span className='icon'>
-              <i className='fab fa-linkedin text links'></i>
+            <span className="icon">
+              <i className="fab fa-linkedin text links"></i>
             </span>
           </Nav.Link>
         </div>
